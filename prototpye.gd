be0dfc746c60d9fb77a1d6a4eed0ac3ce24201be
PrestonTGD <Prestonthall06@gmail.com>
2025-01-29 22:26:18 -0700

    CHANGING

--- prototpye.gd
+++ prototpye.gd
@@ -8,8 +8,6 @@ func _ready() -> void:
 	day_night_shader.time_tick.connect(time_ui.set_daytime)
 
 
-
-func _on_button_pressed() -> void:
-	
+func _on_texture_button_pressed() -> void:
 	var gui_node = get_tree().root.get_node("GamePrototype/Gui")
-	gui_node.add_child(GameController.fishepedia.instantiate())
+	gui_node.add_child(GameController.fishepedia.instantiate())# Replace with function body.
